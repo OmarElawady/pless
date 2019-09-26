acb1d8e346a194b73c953bf5292127ececdf48af
Omar Elawady <omarelawady1998@gmail.com>
2019-09-26 22:52:32 +0200

    Add chess pieces and board image data

---
+++ pless/data.py
@@ -0,0 +1,21 @@
+
+image_dir = '../images/'
+
+background = 'brown.png'
+
+black = {
+        'knight': 'knightb.png',
+        'king': 'kingb.png',
+        'bishop': 'bishopb.png',
+        'queen': 'queenb.png',
+        'rook': 'rookb.png',
+        'pawn': 'pawnb.png',
+        }
+white = {
+        'knight': 'knightw.png',
+        'king': 'kingw.png',
+        'bishop': 'bishopw.png',
+        'queen': 'queenw.png',
+        'rook': 'rookw.png',
+        'pawn': 'pawnw.png',
+        }
